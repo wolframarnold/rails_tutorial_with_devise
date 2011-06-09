f8b99a753809af313e309d10062c1c85a0edff59
Wolfram Arnold <wolfram@rubyfocus.biz>
2011-06-09 15:17:15 -0500

    Chapter 12 (partial): Add Relationship join model Add associations No UI's yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,26 @@ class User < ActiveRecord::Base
 
   has_many :microposts, :dependent => :destroy
 
+  has_many :relationships, :foreign_key => "follower_id",
+                           :dependent => :destroy
+
+  has_many :following, :through => :relationships, :source => :followed
+
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+           :class_name => "Relationship",
+           :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
+
+
+  def following?(followed)
+    relationships.exists?(:followed_id => followed.id)
+  end
+
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
+  end
 end
