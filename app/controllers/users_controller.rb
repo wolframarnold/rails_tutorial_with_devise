19cdfbdae6c91f01ef6cde6aa3c2c8412a17544f
Wolfram Arnold <wolfram@rubyfocus.biz>
2011-07-09 15:32:57 -0700

    removed UsersController#index -- the tutorial covers this in Chapte 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,5 @@
 class UsersController < ApplicationController
 
-  def index
-    @users = User.all
-    @title = "All Users"
-  end
-
   def show
     @user = User.find(params[:id])
     @title = @user.name
