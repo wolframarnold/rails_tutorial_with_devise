5d3179389cd04e58e8358318981de8e7709084a8
Wolfram Arnold <wolfram@rubyfocus.biz>
2011-07-17 15:43:41 -0700

    Added code back in to make specs pass.
    Revert "Removed code so that specs will fail:"
    This reverts commit a08b42f1533c8d4c61a858384a4b6c0ec063f1f1.
    
    From here, commits for further chapters should be red/green style.
    
    Fix sign_up link on home page

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ SampleApp::Application.routes.draw do
   resources :users, :only => [:index, :show]
 
   match '/contact', :to => 'pages#contact'
-
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  
   root :to => 'pages#home'
 end
