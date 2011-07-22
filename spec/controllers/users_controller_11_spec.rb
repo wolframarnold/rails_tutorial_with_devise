a3a3cc1b18585722b9ae109c9b3a0e4251c222d3
Wolfram Arnold <wolfram@rubyfocus.biz>
2011-07-21 19:25:33 -0700

    Chapter 11 specs: microposts

---
+++ spec/controllers/users_controller_11_spec.rb
@@ -0,0 +1,21 @@
+require "rspec"
+
+describe UsersController do
+  render_views
+
+  describe "GET 'show'" do
+
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    it "should show the user's microposts" do
+      mp1 = Factory(:micropost, :user => @user, :content => "Foo bar")
+      mp2 = Factory(:micropost, :user => @user, :content => "Baz quux")
+      get :show, :id => @user
+      response.should have_selector("span.content", :content => mp1.content)
+      response.should have_selector("span.content", :content => mp2.content)
+    end
+  end
+
+end
